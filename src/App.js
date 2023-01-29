2e31648a5d968c7cad4933a637f63ed371962384
geteverything <uk252811@gmail.com>
2023-01-30 01:52:02 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,168 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from "react";
+
+function App() {
+
+  const [result, SetResult] = useState("");
+
+  const clickhandler = (event) => {
+    SetResult(result.concat(event.target.value))
+
+  }
+  const calculate = () => {
+    SetResult(eval(result).toString())
+  }
+  const cleandata = () => {
+    SetResult("");
+  }
+  return (
+    <>
+    <h1>Smart Calculator In React</h1>
+    <div className="container">
+      <div className="calc-body">
+        <div className="calc-screen">
+          <div className="calc-operation">
+         <h3  style={{textAlign:"right", margin:"-8px -5px", fontSize:"30px" }}> {result}</h3>
+         </div>
+           </div>
+        <div className="calc-button-row">
+          <button className="button l"
+            type="button"
+            value="Clear"
+
+            onClick={cleandata}
+
+          >C</button>
+          <button className="button l"
+            type="button"
+            value="-"
+
+            onClick={clickhandler}
+
+          >-</button>
+          <button className="button l"
+            type="button"
+            value="%"
+
+            onClick={clickhandler}
+
+          >%</button>
+           <button className="button l"
+            type="button"
+            value="/"
+
+            onClick={clickhandler}
+
+          >/</button>
+        </div>
+        <div className="calc-button-row">
+          <button className="button"
+            type="button"
+            value="7"
+            onClick={clickhandler}
+
+          >7</button>
+
+          <button className="button"
+            type="button"
+            value="8"
+
+            onClick={clickhandler}
+
+          >8</button>
+          <button className="button"
+            type="button"
+            value="9"
+            onClick={clickhandler}
+
+          >9</button>
+          <button className="button l"
+            type="button"
+            value="*"
+
+            onClick={clickhandler}
+
+          >x</button>
+        </div>
+        <div className="calc-button-row">
+          <button className="button"
+            type="button"
+            value="4"
+            onClick={clickhandler}
+
+          >4</button>
+          <button className="button"
+            type="button"
+            value="5"
+            onClick={clickhandler}
+
+          >5</button>
+          <button className="button"
+            type="button"
+            value="6"
+            onClick={clickhandler}
+
+          >6</button>
+          <div className="button l"> ? </div>
+        </div>
+        <div className="calc-button-row">
+          <button className="button"
+            type="button"
+            value="1"
+            onClick={clickhandler}
+
+          >1</button>
+          <button className="button"
+            type="button"
+            value="2"
+            onClick={clickhandler}
+
+          >2</button>
+          <button className="button"
+            type="button"
+            value="3"
+            onClick={clickhandler}
+
+          >3</button>
+
+
+          <button className="button l"
+            type="button"
+            value="+"
+
+            onClick={clickhandler}
+
+          >+</button>
+        </div>
+        <div className="calc-button-row">
+          <button className="button0"
+            type="button"
+            value="0"
+            onClick={clickhandler}
+
+          >0</button>
+          <button className="button"
+            type="button"
+            value="."
+
+            onClick={clickhandler}
+
+          >.</button>
+
+
+          <button className="button l"
+            type="button"
+            value="="
+
+            onClick={calculate}
+
+          >=</button>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
